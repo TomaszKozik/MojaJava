b8f56ffc68af4e56765a221e8c65373aec3f8cfe
Tomasz Kozik <tkozik@gbox.pl>
2020-05-24 09:52:59 +0200

    Poprawki dla ScannerExample

--- src/main/java/string/ScannerExample.java
+++ src/main/java/string/ScannerExample.java
@@ -3,27 +3,80 @@ package string;
 import java.util.Locale;
 import java.util.Scanner;
 
+/**
+ * Zastosowanie: Pobranie od użytkownika danych wprowadzanych z klawiatury
+ *
+ * Wymagana biblioteka: java.util.Scanner
+ */
 public class ScannerExample {
 
     public static void main(String[] args) {
+
+        /**
+         * Tworzenie obiektu Scanner, który umożliwia korzystania z metod
+         * wprowadzania danych. System.in wskazuje, że program będzie czytał
+         * wprowadzany tekst.
+         */
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("---- ustawienie regionu ----");
-        scan.useLocale(Locale.ENGLISH);      //wprowadzanie liczb z kropką jako separatorem
-        //scan.useLocale(Locale.GERMANY);    //wprowadzanie danych z przecinkiem
+        /**
+         * Ustawienia regionalne dla wprowadzanego tekstu.
+         * Stosuje się je np. do definiowana speratara liczb dziesiętnych
+         */
+        scan.useLocale(Locale.FRANCE);
+        // wprowadzanie liczb z przecinkiem jako separatorem np. 50,102
+        scan.useLocale(Locale.ENGLISH);
+        // wprowadzanie liczb z kropką jako separatorem np. 50.102
 
-        System.out.println("---- Stringi ----");
+        /**
+         * Wprowadzanie pojedynczego słowa, liczby jako stringi.
+         * Funkcja przyjmuje tylko jedno słowo lub liczbę wprowadzoną z klawiatury.
+         * Każdy biały znak informuje program o nowym słowie, liczbie.
+         * np. wpisanie 'Syrena' wyświetli 'Syrena'
+         * np. wpisanie 'Syrena 105L' wprowadzi do programu tylko 'Syrena'
+         */
+        System.out.println("---- Wprowadzanie pojedynczego słowa, liczby jako stringi ----");
+        String string1 = scan.next();
+        System.out.println(string1);
+
+        /**
+         * Wprowadzanie dowolnych słów, liczb jako stringi.
+         * Funkcja przyjmuje dowolnie wprowadzony ciąg i w pełni go wyświetli.
+         * np. wpisanie 'Syrena 105L' wprowadzi do programu 'Syrena 105L'
+         */
+        System.out.println("---- Wprowadzanie dowolnych słów, liczb jako stringi ----");
         String string = scan.nextLine();
         System.out.println(string);
 
-        System.out.println("---- int ----");
+        /**
+         * Wprowadzanie liczb jako liczby całkowite int
+         * Funkcja przyjmuje tylko cyfry.
+         * Przekazanie innych znaków lub liczb niecałkowitych spowoduje błąd 'java.util.InputMismatchException'
+         * np. wpisanie '105' wprowadzi do programu '105'
+         */
+        System.out.println("---- Wprowadzanie liczb jako liczby całkowite int ----");
         Integer integer = scan.nextInt();
         System.out.println(integer);
 
-        System.out.println("---- double ----");
+        /**
+         * Wprowadzanie liczb jako liczby dziesięte dobule
+         * Funkcja przyjmuje tylko cyfry.
+         * Przekazanie innych znaków spowoduje błąd 'java.util.InputMismatchException'
+         * np. wpisanie '105' wprowadzi do programu '105'
+         * np. wpisanie '105.5' wprowadzi do programu '105.5' dla ustawień regionalnych ENGLISH
+         * np. wpisanie '105,5' wprowadzi do programu '105.5' dla ustawień regionalnych FRANCE
+         */
+        System.out.println("---- Wprowadzanie liczb jako liczby dziesięte dobule ----");
         Double aDouble = scan.nextDouble();
         System.out.println(aDouble);
 
+        /**
+         * Wprowadzanie wartości logicznej true lub false
+         * Funkcja przyjmuje tylko 'true' lub 'false'.
+         * Przekazanie innych znaków spowoduje błąd 'java.util.InputMismatchException'
+         * np. wpisanie 'treu' wprowadzi do programu 'true'
+         * np. wpisanie 'false' wprowadzi do programu 'false'
+         */
         System.out.println("---- boolean ----");
         Boolean aBoolean = scan.nextBoolean();
         System.out.println(aBoolean);
