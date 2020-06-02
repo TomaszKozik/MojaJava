a7c8bd8a364cafb04ed13b4d8bac88bced368f39
Tomasz Kozik <tkozik@gbox.pl>
2020-06-02 06:45:02 +0200

    OperacjeNaStringach

--- src/main/java/string/OperacjeNaStringach.java
+++ src/main/java/string/OperacjeNaStringach.java
@@ -3,18 +3,18 @@ package string;
 /**
  * Proste operacje na String'ach. Typu wypisanie Stringa, pobranie długości,
  * usuwanie białych znaków, zmiana wielkości liter, podmiana liter
- *
+ * <p>
  * Source: https://hyperskill.org/learn/step/3523
  */
 public class OperacjeNaStringach {
 
     public static void main(String[] args) {
 
-        /* Przykładowe stringi na których będą wykonane poniższe operacje */
+        /* Przykładowe String'i na których będą wykonane poniższe operacje */
         String stringMultiLine = "Mój samochód to FSM Syrena 105L.\nWyprodukowany w 1980r.\nJego długość to 4,04m.";
         String stringOneLine = "Syrena 105L";
 
-        /*---- Drukowanie stringa na ekranie ----*/
+        /*---- Drukowanie String'a na ekranie ----*/
         System.out.println("---- Drukowanie String'a na ekranie ----");
         System.out.println("Drukowanie String'a na ekranie długiego stringa");
         System.out.println(stringMultiLine);
@@ -27,24 +27,28 @@ public class OperacjeNaStringach {
         System.out.println(stringOneLine); // Syrena 105L
 
         /*
-          Pobranie długości stringa
+          Pobranie długości String'a
           Znak nowej linii jest również dodawany do ilości znaków
           .length()
          */
         System.out.println("---- Pobranie długości String'a ----");
         System.out.println("String długi ma długość:");
-        System.out.println(stringMultiLine.length()); // 78
+        int dlugoscString = stringMultiLine.length();
+        System.out.println(dlugoscString); // 78
 
         /*
-          Weryfikacja, czy string jest pusty
+          Weryfikacja, czy String jest pusty
           Zwraca true lub false
           .isEmpty()
          */
         System.out.println("---- Weryfikacja, czy String jest pusty ----");
         System.out.println("String 'Syrena 105L' nie jest pusty, więc mamy:");
-        System.out.println(stringOneLine.isEmpty()); // false
+        boolean czyStringPusty = stringOneLine.isEmpty();
+        System.out.println(czyStringPusty); // false
         System.out.println("String '' jest pusty, więc mamy:");
-        System.out.println("".isEmpty()); // true
+        String pustyStirng = "";
+        boolean czyString2Pusty = pustyStirng.isEmpty();
+        System.out.println(czyString2Pusty); // true
 
         /*
           Zmiana wielkości liter
@@ -53,70 +57,79 @@ public class OperacjeNaStringach {
          */
         System.out.println("---- Zmiana wielkości liter ----");
         System.out.println("String 'Syrena 105L' zmienił się na:");
-        System.out.println(stringOneLine.toUpperCase());// SYRENA 105L
+        String duzeLitery = stringOneLine.toUpperCase();
+        System.out.println(duzeLitery);// SYRENA 105L
         System.out.println("String 'Syrena 105L' zmienił się na:");
-        System.out.println(stringOneLine.toLowerCase()); // syrena 105l
+        String maleLitery = stringOneLine.toLowerCase();
+        System.out.println(maleLitery); // syrena 105l
 
         /*
-          Weryfikacja zawartości stringa
+          Weryfikacja zawartości String'a
           Zwraca true lub false
           .contains(str)
          */
         System.out.println("---- Weryfikacja zawartości stringa ----");
         System.out.println("Weryfikacja, czy String 'Syrena 105L' zawiera String 'ren'. Wynik to:");
-        System.out.println(stringOneLine.contains("ren")); // true
+        boolean czyZawiera = stringOneLine.contains("ren");
+        System.out.println(czyZawiera); // true
 
         /*
-          Wycięcie fragmentu stringa w zależności od pozycji znaku
-          .substring(int beginIndex) - usuń od indeksu do końca stringa
+          Wycięcie fragmentu String'a w zależności od pozycji znaku
+          .substring(int beginIndex) - usuń od indeksu do końca String'a
           .substring(int beginIndex, int endIndex) - usuń do indeksu do indeksu
          */
-        /*----  ----*/
         System.out.println("---- Wycięcie fragmentu String'a w zależności od pozycji znaku ----");
         System.out.println("Wytnij z stringa 'Syrena 105L' fragment od indexu 7 do 10");
-        System.out.println(stringOneLine.substring(7, 10)); // 105
+        String krotszyString = stringOneLine.substring(7, 10);
+        System.out.println(krotszyString); // 105
 
         /*
-          Zamiana framgentu stringa na inny
+          Zamiana framgentu String'a na inny
           .replace(String str, String str)
          */
         System.out.println("---- Zamiana framgentu String'a na inny ----");
         System.out.println("Zamień w stringu 'Syrena 105L' fragment '105L' na 'Bosto'");
-        System.out.println(stringOneLine.replace("105L", "Bosto")); // 'Syrena Bosto'
+        String zmianaWStringu = stringOneLine.replace("105L", "Bosto");
+        System.out.println(zmianaWStringu); // 'Syrena Bosto'
 
         /*
-          Zamiana framgentu stringa na inny w zaleźności od wyrażenia regularnego
+          Zamiana framgentu String'a na inny w zaleźności od wyrażenia regularnego
           .replaceAll(String regex, String replacement)
           Zobacz: https://regexr.com/
          */
         System.out.println("---- Zamiana znaków zgodnie z wyrażeniem regularnym (regex) ----");
         System.out.println("Z stringa 'Syrena 105L' usuń wszystkie cyfry. Wynik to:");
-        System.out.println(stringOneLine.replaceAll("\\d", "")); // Syrena L
+        String zmianaWStringuZRegex = stringOneLine.replaceAll("\\d", "");
+        System.out.println(zmianaWStringuZRegex); // Syrena L
 
         /*
-          Usuwanie białych znaków z początku i końca stringa
+          Usuwanie białych znaków z początku i końca String'a
           .trim()
          */
         System.out.println("---- Usuwanie białych znaków z początku i końca String'a ----");
         System.out.println("Z stringa 'Syrena 105L' usuń białe znaki z obu jego końców");
-        System.out.println(stringOneLine.trim()); // Syrena 105L
+        String stringBezBialychZnakow = stringOneLine.trim();
+        System.out.println(stringBezBialychZnakow); // Syrena 105L
 
         /*
-          Łączenie stringów
+          Łączenie String'ów
           używa się operatora '+' lub .concat(String str)
          */
         System.out.println("---- Laczenie String'ów ----");
         System.out.println("Do stringa 'Syrena 105L' dołącz za pomocą '.concat()' string ' o kolorze kość słoniowa':");
-        System.out.println(stringOneLine.concat(" o kolorze kość słoniowa")); // Syrena 105L o kolorze kość słoniowa
+        String polaczonyStringConcat = stringOneLine.concat(" o kolorze kość słoniowa");
+        System.out.println(polaczonyStringConcat); // Syrena 105L o kolorze kość słoniowa
         System.out.println("Do stringa 'Syrena 105L' dołącz za pomocą '+' string ' o kolorze wiśni':");
-        System.out.println(stringOneLine + " o kolorze wiśni"); // Syrena 105L o kolorze wiśni
+        String polaczonyStringPlus = stringOneLine + " o kolorze wiśni";
+        System.out.println(polaczonyStringPlus); // Syrena 105L o kolorze wiśni
 
         /*
-          Pobieranie hashCode dla stringa
+          Pobieranie hashCode dla String'a
           .hashCode()
          */
         System.out.println("---- Pobieranie hashCode dla String'a ----");
         System.out.println("hashCode dla stringa 'Syrena 105L' to:");
-        System.out.println(stringOneLine.hashCode()); // 561646090
+        int hashCode = stringOneLine.hashCode();
+        System.out.println(hashCode); // 561646090
     }
 }
