418a1bd645897c60fea0a438d02569a205a279aa
Tomasz Kozik <tkozik@gbox.pl>
2020-06-08 12:43:20 +0200

    Refleksje

---
+++ src/main/java/klasyIMetody/refleksje/RefleksjePrzyklady.java
@@ -0,0 +1,116 @@
+package klasyIMetody.refleksje;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+/**
+ * Refleksja to proces uzyskiwania dostępu i modyfikowania aplikacji w czasie wykonywania.
+ * Dzięki refleksji możesz np. wyświetlić wszystkie nazwy i typy klas, zmiennych, metod,
+ * konstruktorów i ich elementów. Możesz je również modyfikować
+ *
+ * Dzięki refleksji wszystkie pola klas, nawet prywatne, stają się widoczne,
+ * dzięki czemu można je zapisać w pliku zewnętrznym. Często jest to wykorzystywane
+ * w serializacji obiektów
+ *
+ * Dzięki refleksji jest możliwa introspekcja kodu w celu wykrycia adnotacji.
+ * Tak właśnie przetwarzane są prawie wszystkie adnotacje w Javie.
+ *
+ * Źródło: https://hyperskill.org/learn/step/3609
+ * Źródło: https://hyperskill.org/learn/step/8108
+ */
+public class RefleksjePrzyklady {
+
+    public static void main(String[] args) {
+        PrzykladyPobieraniaDanych();
+    }
+
+    /**
+     * Metody zwracające - to metody dzięki którym zwracane są dane dotyczące klas i ich ciał.
+     * Na ich podstawie możemy wyciągać i modyfikować zmienne, metody, konstruktory
+     */
+    public static void MetodyZwracajace() throws ClassNotFoundException {
+        // Metody zwracające klasy - na podstawie nazwy klasy, jako String
+        // Wymaga throws ClassNotFoundException
+        Class studentClass = Class.forName("Samochody");
+
+        // Metody zwracające klasy - dla klasy nadrzędnej
+        String.class.getSuperclass();
+
+        // Metody zwracające klasy - zwraca wszystko z danej i nadrzędnych klas, interfejsów, enum'ów
+        String.class.getClasses();
+
+        // Metody zwracające klasy - zwraca wszystko z danej konkretnej klasy, interfejsu, enuma
+        String.class.getDeclaredClasses();
+    }
+
+    /**
+     * Przykłady pobiernia danych
+     */
+    public static void PrzykladyPobieraniaDanych() {
+        Samochody_KlasaDoPrzykladow samochody = new Samochody_KlasaDoPrzykladow();
+        Class samochod = samochody.getClass();
+
+        // Pobieranie danych o kontruktorze danej klasy
+        Constructor[] declaredConstructors = samochod.getDeclaredConstructors();
+        // Pobieranie danych o kontruktorze danej klasy i nadrzędnych
+        Constructor[] constructors = samochod.getConstructors();
+        // Pobieranie danych o polach danej klasy
+        Field[] declaredFields = samochod.getDeclaredFields();
+        // Pobieranie danych o polach danej klasy i nadrzędnych
+        Field[] fields = samochod.getFields();
+        // Pobieranie danych o metodach danej klasy
+        Method[] declaredMethods = samochod.getDeclaredMethods();
+        // Pobieranie danych o metodach danej klasy i nadrzędnych
+        Method[] methods = samochod.getMethods();
+
+        for(Constructor dc : declaredConstructors) {
+            System.out.println("Zdeklarowany konstruktor danej klasy: " + dc.getName());
+        }
+        for (Constructor c : constructors) {
+            System.out.println("Zdeklarowany konstruktor danej klasy i nadrzędnych: " + c.getName());
+        }
+        for (Field df : declaredFields) {
+            System.out.println("Zdeklarowane pole danej klasy: " + df.getName());
+        }
+        for (Field f : fields) {
+            System.out.println("Zdeklarowane pole danej klasy i nadrzędnych: " + f.getName());
+        }
+        for (Method dm : declaredMethods) {
+            System.out.println("Zdeklarowana metoda danej klasy: " + dm.getName());
+        }
+        for (Method m : methods) {
+            System.out.println("Zdeklarowana metoda danej klasy i nadrzędnych: " + m.getName());
+        }
+
+
+        // Istnieje poszerzenie pobierania informacji o modyfikatory dostępu
+        // Źródło: https://hyperskill.org/learn/step/8104
+        System.out.println("---- Pobieranie pełnych informacji o metodach ----");
+        Field[] fieldsOfStudent = samochod.getDeclaredFields();
+        for (Field field : fieldsOfStudent) {
+            int modifiers = field.getModifiers(); // wymagana zmienna do możliwości określenia modyfikatora
+            if (Modifier.isPublic(modifiers)) {
+                System.out.print("public ");
+            }
+            if (Modifier.isProtected(modifiers)) {
+                System.out.print("protected ");
+            }
+            if (Modifier.isPrivate(modifiers)) {
+                System.out.print("private ");
+            }
+            if (Modifier.isStatic(modifiers)) {
+                System.out.print("static ");
+            }
+            if (Modifier.isFinal(modifiers)) {
+                System.out.print("final ");
+            }
+
+            System.out.print(field.getType() + " ");
+            System.out.println(field.getName());
+        }
+    }
+
+
+}
---
+++ src/main/java/klasyIMetody/refleksje/WeryfikacjaInstancji.java
@@ -0,0 +1,125 @@
+package klasyIMetody.refleksje;
+
+/**
+ * Weryfikacja instancji
+ * Dzięki refleksji i zamiennie metodzie instanceof możemy w łatwy
+ * sposób zweryfikować, czy instancja danej klasy została zdeklarona,
+ * a tym samym sprawdzić ile razy.
+ */
+public class WeryfikacjaInstancji
+{
+    static class Przyklady {
+        public static void main(String[] args) {
+            // Instancje na potrzeby przykładów
+            Samochod samochod = new Samochod("Syrena", "kosc sloniowa", 4, "sedan", 4);
+            Rower rower = new Rower("Giant", "niebieski", 2, 21, "tarczowe");
+
+            // instaceof zwraca nam informację, czy dana instancja dla danej klasy została utworzona
+            // W przypadku instance of brane są pod uwagę również klasy nadrzędne
+            boolean rowerToRower = rower instanceof Rower; // true
+            // Błąd kompatybilności typu:
+            // boolean rowerToSamochod = rower instanceof Samochod; // false
+            boolean rowerToPojazd = rower instanceof Pojazd; // true
+
+            // Refleksja zwraca nam informację, czy dana instancja dla danej klasy została utworzona
+            // W przypadku refleksji mamy informację dotyczącą wyłącznie konrektnej klasy
+            boolean rowneRower = rower.getClass() == Rower.class; // true
+            // Błąd kompatybilności typu:
+            // boolean rownePojazd = rower.getClass() == Pojazd.class;   // false
+            // Błąd kompatybilności typu:
+            // boolean rowneSamochod = rower.getClass() == Samochod.class; // false
+        }
+    }
+
+
+    /**
+     * Zadanie 1
+     * Sprawdź instancje
+     */
+    static class Zadanie1 {
+        public static void main(String[] args) {
+            Pojazd pojazd1 = new Pojazd("EMP", "w", 23);
+            Pojazd pojazd2 = new Pojazd("EMP2", "w2", 2);
+            Pojazd pojazd3 = new Samochod("EMP3", "w3", 44, "asf", 5);
+            Pojazd[] pojazdy = {pojazd1, pojazd2, pojazd3};
+            determineWhoIsWho(pojazdy);
+        }
+
+        public static void determineWhoIsWho(Pojazd[] pojazdy) {
+            for (Pojazd pojazd : pojazdy) {
+                if (pojazd.getClass() == Samochod.class)
+                    System.out.println("Samochod");
+                else if (pojazd.getClass() == Rower.class)
+                    System.out.println("Rower");
+                else
+                    System.out.println("Pojazd");
+            }
+        }
+    }
+}
+
+// Klasy do przykładów
+class Pojazd {
+
+    protected String marka;
+    protected String kolor;
+    protected int iloscKol;
+
+    public Pojazd(String marka, String kolor, int iloscKol) {
+        this.marka = marka;
+        this.kolor = kolor;
+        this.iloscKol = iloscKol;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public String getKolor() {
+        return kolor;
+    }
+
+    public int getIloscKol() {
+        return iloscKol;
+    }
+}
+
+class Samochod extends Pojazd {
+
+    private String typNadwozia;
+    private int iloscOsob;
+
+    public Samochod(String name, String email, int experience, String typNadwozia, int iloscOsob) {
+        super(name, email, experience);
+        this.typNadwozia = typNadwozia;
+        this.iloscOsob = iloscOsob;
+    }
+
+    public String getTypNadwozia() {
+        return typNadwozia;
+    }
+
+    public int getIloscOsob() {
+        return iloscOsob;
+    }
+}
+
+class Rower extends Pojazd {
+
+    private int iloscPrzezutek;
+    private String rodzajHamulca;
+
+    public Rower(String name, String email, int experience, int iloscPrzezutek, String rodzajHamulca) {
+        super(name, email, experience);
+        this.iloscPrzezutek = iloscPrzezutek;
+        this.rodzajHamulca = rodzajHamulca;
+    }
+
+    public int IloscPrzezutek() {
+        return iloscPrzezutek;
+    }
+
+    public String getRodzajHamulca() {
+        return rodzajHamulca;
+    }
+}
