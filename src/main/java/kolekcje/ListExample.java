ea5e5399625df4609e2f92681cec0d8462bd3c6c
Tomasz Kozik <tkozik@gbox.pl>
2020-06-21 15:25:46 +0200

    ListExample

---
+++ src/main/java/kolekcje/ListExample.java
@@ -0,0 +1,67 @@
+package kolekcje;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+
+/**
+ * Lista to uporządkowana kolekcja elementów. Oznacza to, że każdy element ma pozycję
+ * na liście określoną indeksem całkowitym, tak jak w zwykłych tablicach.
+ *
+ * List<E> tworzy się na bazie interfejsu Collection<E>
+ *
+ * Są dwa rodzaje list:
+ * - niemodyfikowalne (immutable) - tworzy się na podstawie nieinstancyjnych kolekcji,
+ *   nie możemy ich edytować, dodawać do nich lub usuwać z nich rekordów
+ * - modyfikowalne (mutable) - tworzy się na podstawie instancyjnych kolekcji,
+ *   na listach modyfikowalnych działają wszystkie operacje, jak dla ArrayList<E>
+ */
+public class ListExample {
+    public static void main(String[] args) {
+
+        // Lista niemodyfikowalna na podstawie listy elementów
+        List<String> listaNiemodyfikowalna1 = asList("Syrena", "Fiat", "Warszawa");
+
+        // Lista niemodyfikowalna na podstawie tablicy
+        String[] tablicaSamochodow = {"Syrena", "Fiat", "Warszawa"};
+        List<String> listaNiemodyfikowalna2 = Arrays.asList(tablicaSamochodow);
+
+        // Lista niemodyfikowalna na podstawie listy elementów (od Java 9)
+        // List<String> listaNiemodyfikowalna3 = List.of("Syrena", "Fiat", "Warszawa");
+
+        // Lista niemodyfikowalna na podstawie klasy Collections
+        List<String> listaNiemodyfikowalna4 = Collections.emptyList();
+        List<Integer> listaNiemodyfikowalna5 = Collections.singletonList(100);
+
+        // Przekształce listy modyfikowalnej w niemodyfikowalną
+        List<String> listaModyfikowalna1 = new ArrayList<>(Arrays.asList(tablicaSamochodow));
+        List<String> listaNiemodyfikowalna6 = Collections.unmodifiableList(listaModyfikowalna1);
+
+
+        // Lista modyfikowalna na podstawie new ArrayList
+        List<String> listaModyfikowalna2 = new ArrayList<>(); // pusta lista modyfikowalna
+
+        // Przekształce listy niemodyfikowalnej w modyfikowalną
+        String[] listaNiemodyfikowalna7 = {"Syrena", "Fiat", "Warszawa"};
+        List<String> listaModyfikowalna3 = new ArrayList<>(Arrays.asList(listaNiemodyfikowalna7));
+
+
+
+        //  Sortowanie listy
+
+        //Lista do poniższych przykładów:
+        List<String> listaSamochodow = asList("Syrena", "Fiat", "Warszawa");
+
+        // stream - tworzy nową mutowalną listę
+        List<String> sortowanieStream = listaSamochodow.stream().sorted().collect(Collectors.toList());
+        sortowanieStream.forEach(System.out::println);
+
+        // Collections - nie zmienia rodzaju listy
+        Collections.sort(listaSamochodow);
+        listaSamochodow.forEach(System.out::println);
+    }
+}
--- src/main/java/kolekcje/RodzajeKolekcji.java
+++ src/main/java/kolekcje/RodzajeKolekcji.java
@@ -1,77 +1,12 @@
 package kolekcje;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static java.util.Arrays.*;
 
 // todo wrzucić do qahelper.kozik.ovh
 public class RodzajeKolekcji {
-    /**
-     * ArrayList - jest to dynamiczna tablica. Jej ilość elementów może
-     * wzrastać lub zmniejszać się.
-     *
-     * Nie może przechowywać typów pierwotnych
-     *
-     * Wymaga importu biblioteki java.util.ArrayList
-     */
 
-
-    /**
-     * Lista to uporządkowana kolekcja elementów. Oznacza to, że każdy element ma pozycję
-     * na liście określoną indeksem całkowitym, tak jak w zwykłych tablicach.
-     *
-     * List<E> tworzy się na bazie interfesju Collection<E>
-     *
-     * Nie można utworzyć bezpośredniej instancji List<E>. Tworzy się ją na podstawie
-     * ArrayList<E> lub LinkedList<E>
-     *
-     * Są dwa rodzaje list: niezmienne (immutable) i zmienne (mutable)
-     * Niezmienną tworzy się na podstawie nieinstacyjnych kolekcji,
-     * a zmienne na podstawie instacyjnych
-     */
-    static class Kolekcja_List {
-
-        public static void main(String[] args) {
-            /*
-              Rodzaje list
-              Są dwa rodzaje list: niezmienne (immutable) i zmienne (mutable)
-              Niezmienną tworzy się na podstawie nieinstacyjnych kolekcji,
-              a zmienne na podstawie instacyjnych.
-              Na listach niezmiennych nie możemy edytować, dodawać, usuwać rekordów.
-              Na listach zmiennych działają wszystkie operacje, jak dla ArrayList<E>
-             */
-            // Lista niezmienna na podstawie listy elementów
-            List<String> niezmiennaLista = asList("Syrena", "Fiat", "Warszawa");
-
-            // Lista niezmienna na podstawie listy elementów od Javy 9
-            // List<String> niezmiennaListaOdJava9 = List.of("Syrena", "Fiat", "Warszawa");
-
-            // Lista niezmienna na podstawie klasy Collections
-            List<String> niezminnaCollectionsPusta = Collections.emptyList();
-            List<Integer> niezminnaCollectionsZParametrem = Collections.singletonList(100);
-
-            // Lista zmienna na podstawie instacji ArrayList
-            List<String> zmiennaLista = new ArrayList<>();
-
-            // Przekształce listy zmiennej w niezmienną
-            List<String> niezmiennaListaZZmiennejListy = Collections.unmodifiableList(zmiennaLista);
-
-            /**
-             * Sortowanie listy
-             * Znam dwa proste sposoby sortowania list przez strem i Collections
-             */
-            List<String> listaSamochodow = asList("Syrena", "Fiat", "Warszawa");
-
-            // stream - tworzy nową mutowalną listę
-            List<String> sortowanieStream = listaSamochodow.stream().sorted().collect(Collectors.toList());
-            sortowanieStream.forEach(System.out::println);
-
-            // Collections - pozostawia niemutowalną listę
-            Collections.sort(listaSamochodow);
-            listaSamochodow.forEach(System.out::println);
-        }
-    }
 
     /**
      * Kolekcja Set to zbiór unikatowych elementów.
@@ -192,7 +127,9 @@ public class RodzajeKolekcji {
             Integer[] tablicaInteger = {5, 6, 7, 8};
 
             // Kolekcje niezmienny
-            List<String> listNiezmienny = Arrays.asList("a", "b", "c", "d");
+            List<String> listNiezmienny = Arrays.asList(tablicaString);
+            listNiezmienny.add("e");
+            listNiezmienny.forEach(System.out::println);
 
             /*
               List
