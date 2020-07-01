78541e9afc4667e7eda80e407da7f2063c8d19b9
Tomasz Kozik <tkozik@gbox.pl>
2020-07-01 06:08:44 +0200

    Odniesienia

--- src/main/java/stream/Odniesienia.java
+++ src/main/java/stream/Odniesienia.java
@@ -4,29 +4,37 @@ import java.util.Scanner;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+/**
+ * Odniesienia, to inaczej sposoby zapisu metod lambd, aby je wywołać
+ */
 public class Odniesienia {
+
+    // Metoda do przykładów
+    public static int square(Integer x) {
+        return x * x;
+    }
 
     public static void main(String[] args) {
 
-        // Odniesienie do metody statycznej - przykład dla własnej metody
+        // Odniesienie do metody statycznej - przykład dla własnej metody*
         Function<Integer, Integer> sqr = Odniesienia::square;
         sqr.apply(10); // 100
 
         // Odniesienie do metody statycznej - przykład drugi dla własnej metody
-        Function<Integer, Integer> sqr2 = x -> Odniesienia.square(x);
+        Function<Integer, Integer> sqr2 = x -> square(x);
         sqr2.apply(10); // 100
 
         // Odniesienie do metody statycznej - przykład dla metod z bibliotek
         Function<Double, Double> sqrt = Math::sqrt;
         sqrt.apply(10d); // 100d
 
-        // Odniesienie do niestatycnej metody, do jej instancji
+        // Odniesienie do niestatycznej metody, do jej instancji
         Scanner scanner = new Scanner(System.in);
         Supplier<String> string = scanner::nextLine;
         String pierwszyString = string.get();
         String drugiString = string.get();
 
-        // Odniesienie do niestatycnej metody, do jej instancji
+        // Odniesienie do niestatycznej metody, do jej instancji
         Supplier<String> string2 = () -> scanner.nextLine();
         String trzeciString = string2.get();
 
@@ -41,9 +49,5 @@ public class Odniesienia {
         // Odniesienie do konstruktora
         Supplier<String> newString = String::new;
         Supplier<String> newString2 = () -> new String(); // less readable than the method reference
-    }
-
-    public static int square(Integer x) {
-        return x * x;
     }
 }
