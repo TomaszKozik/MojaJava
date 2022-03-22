59d2717e6ae87fcdb8ada3fecb62a352352b774c
Tomasz Kozik <biuro@funqa.pl>
2022-03-22 06:28:08 +0100

    Clean decorator class: PizzaMaker

--- src/main/java/wzorceProjektowe/decorator/PizzaMaker.java
+++ src/main/java/wzorceProjektowe/decorator/PizzaMaker.java
@@ -1,46 +1,44 @@
 package wzorceProjektowe.decorator;
 
+/**
+ * Decorator pozwala na budowanie wieloskładnikowych obiektów np. pizza. Np. zamiast tworzenia gotowej klasy
+ * stanowiącej gotowy rodzaj pizzy wraz z jej ceną, dekorator umożliwia budowanie tej pizzy. Każda klasa dekoratora
+ * to składnik pizzy wraz z jego ceną. Dzięki czemu jest zwiększona możliwość dokonywania zmian i konfiguracji.
+ * <p>
+ * Źródło wiedzy: https://www.youtube.com/watch?v=j40kRwSm4VE - ang., czas oglądania 12:57
+ */
 public class PizzaMaker {
 
     public static void main(String[] args) {
-        Pizza basicPizza = new TomatoSauce(new Mozzarella(new PlainPizza()));
+        Pizza pizza = new Mozzarella(new TomatoSauce(new Dough()));
 
-        System.out.println(basicPizza.getDescription());
-        System.out.println(basicPizza.getCost());
+        System.out.println("---------");
+        System.out.println("Ready pizza:");
+        System.out.println("  Ingredients: " + pizza.getDescription());
+        System.out.println("  Price: " + pizza.getCost() + "$");
     }
 }
 
 interface Pizza {
+
     String getDescription();
 
     Double getCost();
 }
 
-abstract class ToppingDecorator implements Pizza {
+/**
+ * Podstawowa klasa, którą będziemy dekorować
+ * Jest nią ciasto na pizze
+ */
+class Dough implements Pizza {
 
-    protected Pizza tempPizza;
-
-    public ToppingDecorator(Pizza tempPizza) {
-        this.tempPizza = tempPizza;
+    public Dough() {
+        System.out.println("Prepare dough");
     }
 
     @Override
     public String getDescription() {
-        return tempPizza.getDescription();
-    }
-
-    @Override
-    public Double getCost() {
-        return tempPizza.getCost();
-    }
-}
-
-
-class PlainPizza implements Pizza {
-
-    @Override
-    public String getDescription() {
-        return "Thin douhg";
+        return "Thin dough";
     }
 
     @Override
@@ -49,37 +47,63 @@ class PlainPizza implements Pizza {
     }
 }
 
-class Mozzarella extends ToppingDecorator {
+/**
+ * Klasa abstrakcyjna przyjmująca klasę do dekorowania i pobierająca
+ * podstawowe elementy do dopisywania
+ */
+abstract class PizzaDecorator implements Pizza {
+
+    protected Pizza pizza;
+
+    public PizzaDecorator(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    @Override
+    public String getDescription() {
+        return pizza.getDescription();
+    }
+
+    @Override
+    public Double getCost() {
+        return pizza.getCost();
+    }
+}
+
+/**
+ * Deokoracja 1: mozzarella
+ */
+class Mozzarella extends PizzaDecorator {
 
     public Mozzarella(Pizza tempPizza) {
         super(tempPizza);
-
-        System.out.println("Adding dough");
         System.out.println("Adding mozarella");
     }
 
     public String getDescription() {
-        return tempPizza.getDescription() + ", + mozarella";
+        return pizza.getDescription() + ", mozarella";
     }
 
     public Double getCost() {
-        return tempPizza.getCost() + 0.50;
+        return pizza.getCost() + 0.50;
     }
 }
 
-class TomatoSauce extends ToppingDecorator {
+/**
+ * Dekoracja 2: sos pomidorowy
+ */
+class TomatoSauce extends PizzaDecorator {
 
     public TomatoSauce(Pizza tempPizza) {
         super(tempPizza);
-
         System.out.println("Adding tomato sauce");
     }
 
     public String getDescription() {
-        return tempPizza.getDescription() + ", + tomato souce";
+        return pizza.getDescription() + ", tomato souce";
     }
 
     public Double getCost() {
-        return tempPizza.getCost() + 0.20;
+        return pizza.getCost() + 0.20;
     }
 }
