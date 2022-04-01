f1e53c0905aaa5c439c82fc6b4815b2f004ef1c7
Tomasz Kozik <biuro@funqa.pl>
2022-04-01 06:08:52 +0200

    Refactor Strategy design pattern. Set my own example.

--- src/main/java/wzorceProjektowe/strategy/RunExample.java
+++ src/main/java/wzorceProjektowe/strategy/RunExample.java
@@ -1,76 +1,98 @@
 package wzorceProjektowe.strategy;
-
 /**
- * Interfejs dający pewną możliwość, którą można implementować na wiele różnych sposobów
- * np. opisów, funkcji, warunków do spełnienia.
- * Gotowy interfejs można użyć w klasie wykorzystującej różne klasy implementujące ten interfejs.
- * Dzięki temu unika się
+ * Wzorzec strategy jest wzorcem behawioralnym.
+ * Pozwala na implementowanie podobnych zachowań z listy poprzez deklarację interfejsu,
+ * przy czym:
+ * - w skład listy wchodzi wiele klas implementujących interfejs
+ * - każda klasa ma podobne zachowanie - podobny warunek do spełnienia lub inną funkcję
+ *   np. wydrukowanie zbliżonej treści (coś może latać, coś nie może latać).
+ *
+ * Przydatny szczególnie, gdy mamy wiele obiektów, na których pracujemy,
+ * a które mają podobne warunki do spełnienia.
+ *
+ * Pomoga w:
+ * - ograniczeniu ilości kodu poprzez ograniczenie ilości np. warunków do spełnienia w klasach tego wymagających
  * - duplikacji kodu
- * - redukuje się ilość kodu
- * - chroni przed wymuszaniem zmian w innych klasach
- * Minusem jest zwiększona ilości klas i metod
+ * - chroni przed koniecznymi zmianami w innych klasach
+ * Minusem jest duża ilość klas
+ *
+ * Źródło wiedzy: https://www.youtube.com/watch?v=-NCgRD9-C6o&list=PLF206E906175C7E07&index=3
  */
-interface Fly {
-    String fly();
+
+/**
+ * Interfejs dla wzorca
+ */
+interface TypeOfBuilding {
+    Boolean check(Integer numberOfFlats, Integer numberOfFloors);
 }
 
-class CanFlies implements Fly {
+/**
+ * Pierwszy warunek - sprawdza, czy budynek to dom jednorodzinny
+ */
+class HouseChecker implements TypeOfBuilding {
 
     @Override
-    public String fly() {
-        return "Umie latać";
-    }
-}
-
-class CannotFlies implements Fly {
-
-    @Override
-    public String fly() {
-        return "Nie umie latać";
+    public Boolean check(Integer numberOfFlats, Integer numberOfFloors) {
+        return numberOfFlats == 1;
     }
 }
 
 /**
- * Klasa abstrakcyjna umożliwiająca klasą dziedziczącym ustawianie funkcji, warunków,
- * czyli klas implementujących interfejs.
+ * Drugi warunek - sprawdza, czy budynek jest budynkiem szeregowym
  */
-abstract class Animal {
-    public Fly theAbilityToFly;
+class TerracedBuildingChecker implements TypeOfBuilding {
 
-    public String isFlies() {
-        return theAbilityToFly.fly();
-    }
-
-    public void setTheAbilityToFly(Fly theAbilityToFly) {
-        this.theAbilityToFly = theAbilityToFly;
+    @Override
+    public Boolean check(Integer numberOfFlats, Integer numberOfFloors) {
+        return numberOfFlats > 1 && numberOfFlats == 1;
     }
 }
 
-class Dog extends Animal {
-    public Dog() {
-        theAbilityToFly = new CannotFlies();
-    }
-}
 
-class Stork extends Animal {
-    public Stork() {
-        theAbilityToFly = new CanFlies();
+/**
+ * Pierwszy warunek - sprawdza, czy budynek to blok mieszkalny
+ */
+class BlockOfFlatsChecker implements TypeOfBuilding {
+
+    @Override
+    public Boolean check(Integer numberOfFlats, Integer numberOfFloors) {
+        return numberOfFlats > 1 && numberOfFlats > 1;
     }
 }
 
 /**
- * Przykład obrazuje deklaracje klas zwierząt oraz sprawdzenie na podstawie wzorca
- * strategy, czy wykonują odpowiednie dla nich funkcje, warunki
+ * Klasa wykorzystująca podany powyżej interfejs,
+ * umożliwiająca sprawdzenie rodzaju budynku poprzez `check`
+ * oraz zmianę rodzaju budynku poprzez `setBuildingType`
  */
+abstract class Building {
+    public TypeOfBuilding typeOfBuilding;
+
+    public boolean check(Integer numberOfFlats, Integer numberOfFloors) {
+        return typeOfBuilding.check(numberOfFlats, numberOfFloors);
+    }
+
+    public void setBuildingType(TypeOfBuilding typeOfBuilding) {
+        this.typeOfBuilding = typeOfBuilding;
+    }
+}
+
+/**
+ * Nasz budynek
+ */
+class House extends Building {
+
+    public House() {
+        typeOfBuilding = new HouseChecker();
+    }
+}
+
 public class RunExample {
     public static void main(String[] args) {
-        Animal azor = new Dog();
-        Animal klekotek = new Stork();
-
-        System.out.println("Czy Klekotek lata? " + klekotek.isFlies());
-        System.out.println("Czy Azor lata? " + azor.isFlies());
-        azor.setTheAbilityToFly(new CanFlies());
-        System.out.println("Czy Azor lata? " + azor.isFlies());
-
+        Building house = new House();
+        System.out.println("Is it a real house? " + house.check(1, 5));
+        System.out.println("Change house to the terrace building");
+        house.setBuildingType(new TerracedBuildingChecker());
+        System.out.println("Is it still a house? " + house.check(1, 5));
     }
 }
